094dedcdd63ec450e885bd4751e0b4751ee5cd73
Oscar Sura <surao@stanford.edu>
2018-09-08 08:41:26 -0700

    Testing Python entry point

--- renderer.py
+++ renderer.py
@@ -1,7 +1,9 @@
-print("Hello")
+def start(func):
+    func()
 
-def main():
-    
-if __name__ == '__main__':
-    main()
+def example():
+    print('this is a parameterless function')
 
+if __name__ == 'renderer':
+    print('run as a module!')
+    start(example)
